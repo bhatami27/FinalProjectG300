fc76d4cab44b4d6513bdc86111141bbb8d2bcdba
Bryan Hatami <bryanhatami27@gmail.com>
2021-12-02 21:16:49 -0500

    Had to redo all work that was complete
    Build New Level
    Start Menu
    Scene Management
    Puzzle 1
    Enemy 1
    Enemy 2
    Enemy sound
    Health
    Player EndGame Point
    Doors
    Open Doors
    Trigger event for Doors
    trigger even for puzzle (bugs)

---
+++ Vintage Von/Assets/BoxPuzzle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoxPuzzle : MonoBehaviour
+{
+    public Transform player;
+    private int currentSceneIndex;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            LoadPuzzle();
+        }   
+    }
+
+    void LoadPuzzle()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        PlayerPrefs.SetInt("SaveScene", currentSceneIndex);
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Vintage Von/Assets/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public float timeValue = 90;
+    public Text timeText;
+    // Start is called before the first frame update
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeValue >0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0; 
+        }
+
+        DisplayTime(timeValue);
+    
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if(timeToDisplay<0)
+        {
+            timeToDisplay = 0;
+        }
+        float min = Mathf.FloorToInt(timeToDisplay/60);
+        float sec = Mathf.FloorToInt(timeToDisplay%60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", min, sec);
+    }
+}
